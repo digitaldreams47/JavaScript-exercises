561a1539dd23e5e9871f28db76a3067e254df5a7
Hila Serban <digitaldreams553@gmail.com>
2023-04-03 19:26:27 +0300

    Checked property

--- index.js
+++ index.js
@@ -232,7 +232,7 @@ console.log(letter);
 
 */
 
-/* if else statements */
+/* if else statements 
 
  let age = -1;
 
@@ -253,4 +253,32 @@ if (online) {
   console.log("You are not online");
 }
 
+*/
+
+// Checked property 
+
+
+document.getElementById("myBtn").onclick = function() {
+  const myCheckBox = document.getElementById("myCheckBox");
+  const visaBtn = document.getElementById("visaBtn");
+  const mastercardBtn = document.getElementById("mastercardBtn");
+  const paypalBtn = document.getElementById("paypalBtn");
+
+  if (myCheckBox.checked) {
+    console.log("You are subscribed");
+  }
+  else {
+    console.log("You are not subscribed");
+  }
+
+  if (visaBtn.checked) {
+    console.log("You are paying with visa card");
+  } else if (mastercardBtn.checked) {
+    console.log("You are paying with mastercard");
+  } else if (paypalBtn.checked) {
+    console.log("You are paying with PayPal");
+  } else {
+    console.log("You must select a payment method!")
+  }
+}
 
