9789afbe7c6d0f9b363fc2ac75fc23b2a8e17435
Hila Serban <digitaldreams553@gmail.com>
2023-04-03 15:42:49 +0300

    Basic variables

---
+++ index.js
@@ -0,0 +1,34 @@
+// Variables
+
+// let studentAge = 21; // number
+// let firstName = "Hila Serban"; // numbers
+// let student = false; // boolean
+
+// studentAge += 1;
+
+
+// console.log("Hello " + firstName);
+// console.log("You are: " + studentAge + " years old");
+// console.log("Enrolled: " + student);
+
+// document.getElementById("p1").innerHTML = "Hello, " + firstName;
+// document.getElementById("p2").innerHTML = "You are, " + studentAge + " years old";
+// document.getElementById("p3").innerHTML = "Enrolled: " + student;
+
+// Arithmetic expressions
+
+let students = 20;
+
+// students += 1;
+// students -= 1;
+// students *= 1;
+// students /= 1;
+let extraStudents;
+extraStudents = students % 3;
+
+console.log("students: " + students);
+console.log("extraStudents: " + extraStudents);
+
+
+let result = 1 + 2 * (3 + 4);
+console.log("result: " + result);
