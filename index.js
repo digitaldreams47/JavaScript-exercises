c5f5464e627c6bf2f7f6683c1a68230b011ff318
Hila Serban <digitaldreams553@gmail.com>
2023-04-04 21:48:28 +0300

    shuffle array

--- index.js
+++ index.js
@@ -744,3 +744,25 @@ function print(element){
 
 */
 
+/* shuffle array 
+
+let cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+shuffle(cards);
+// console.log(cards);
+cards.forEach(card => console.log(card));
+
+function shuffle(array) {
+  let currentIndex = array.length;
+
+  while (currentIndex != 0) {
+    let randomIndex = Math.floor(Math.random() * array.length);
+    currentIndex--;
+
+    let temp = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temp;
+  }
+  return array;
+}
+*/
