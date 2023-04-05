a18acf30757ed0af09a9f224802948d89e7e9e11
Hila Serban <digitaldreams553@gmail.com>
2023-04-05 16:39:27 +0300

    setInterval and setTimeout function

--- index.js
+++ index.js
@@ -1193,3 +1193,50 @@ finally {
 */
 
 
+/* setTimeout() invoke a function after a number of milliseconds
+                asynchronous function (doesn t pause execution) 
+
+let item = "cryptocurrency";
+let price = 421;
+
+let timer1 = setTimeout(firstMessage, 3000, item, price);
+let timer2 = setTimeout(secondMessage, 6000);
+let timer3 = setTimeout(thirdMessage, 9000);
+
+function firstMessage(item, price){
+  alert(`Buy this ${item} for $${price}`);
+}
+function secondMessage(){
+  alert(`Buy this course for 500$`)
+}
+function thirdMessage(){
+  alert(`This is not a scam`)
+}
+
+document.getElementById("buyBtn").onclick = function() {
+  clearTimeout(timer1);
+  clearTimeout(timer2);
+  clearTimeout(timer3);
+  alert(`Do it!`);
+}
+
+*/
+
+/* setInterval() invokes a function repeatedly after a number of milliseconds 
+
+let count = 0;
+let max = window.prompt("Count up to number?")
+max = Number(max);
+
+const myTimer = setInterval(countUp, 1000);
+
+function countUp() {
+  count++;
+  console.log(count);
+  if (count >= max) {
+    clearInterval(myTimer);
+  }
+}
+
+*/
+
